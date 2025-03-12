0949f66b126477afe9b1f65bcc8470526d15fa48
bhaktraj <ankitbhaktraj@gmail.com>
2025-03-12 13:04:43 +0530

    add minikubeinstall scripts

---
+++ installminikubeandkubectl.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+sudo apt update
+#install docker
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+# add user in docker Group
+sudo usermod -aG docker $USER && newgrp docker
+
+
+#install minikube
+sudo apt update
+curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+
+#install Kubectl
+sudo snap install kubectl --classic
